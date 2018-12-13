1db9955c859d1311f92bfa7c62c8435b146c4a42
Bharghav <bharghav@happay.in>
2018-12-13 22:07:42 +0530

    Initial commit with pod spec

---
+++ ImageClearScanner/ImageClearScanner-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ICSCropViewController.h"
+//#import "OpenCVWrapper.h"
+//#import "UIImageView+ContentFrame.h"
+//#import "MACaptureSession.h"
+//#import "UIImage+fixOrientation.h"
+//#import "MAImagePickerFinalViewController.h"
+//#import "MAConstants.h"
+//#import "MAOpenCV.h"
+//#import "MAImagePickerControllerAdjustViewController.h"
+//#import "MADrawRect.h"
+//#import "MAImagePickerController.h"
--- ImageClearScanner/ViewController.swift
+++ ImageClearScanner/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ICSCropDelegate {
+    
+    @IBOutlet weak var sourceImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func detectEdges() {
+        
+    }
+    
+    @IBAction func cropButtonClicked(_ sender: UIBarButtonItem) {
+        let vc = ICSCropViewController()
+        vc.cropdelegate = self
+        let nc = UINavigationController(rootViewController: vc)
+        vc.adjustedImage = sourceImageView.image!
+        self.present(nc , animated: true) {
+            //TODO
+        }
+    }
 
+    func didFinishCropping(_ finalCropImage: UIImage, from cropObj: ICSCropViewController) {
+        sourceImageView.image = finalCropImage
+        self.dismiss(animated: true) {
+            //TODO
+        }
+    }
 }
 
